aa0d436a40133f2dd771f06f9bd415a9e10df87d
scottlurowist <scottlurowist@gmail.com>
2020-10-17 09:33:42 -0400

    I HAVE A WORKING HEADER BAR
    In package.json, I reinstalled enzyme and the react 16 enzyme adapter
    as dev dependencies, not production dependencies.
    
    App.jsx has been modified to use react router. I thought that I
    included it as part of the previous commit but apparently I did not.
    It is now has a single route to home page.
    
    In index.scss, I set using the box-sizing to border-box using the
    universal selector , and set the font-size to 16px so that I could
    use the REM system for specifying sizes.
    
    App.spec.jsx had a typo in its header; I called the file “.js”. This
    has been changed to “.jsx”.
    
    I added the components folder and added the header component. I want
    to get that working first. This is also the first component that I have
    styled. I also included the images for GitHub, Linked In, and Medium
     where I shall blog. So, there is a portfolio link, and links for
     Github, LinkedIn and Medium.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,34 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+// App.jsx
+//
+// The entry point for the portfolio app. it is invoked by index.js.
+//
+////////////////////////////////////////////////////////////////////////////////
 
 
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+
+import HomePage from './pages/home/HomePage-component';
+import Header from './components/header/Header-component';
+
 import './App.scss';
 
+
+
+
+// The entry point for the portfolio app. 
 function App() {
   return (
-    <div className="App">HELLO WORLD!</div>
+    <div>
+      <Header />
+      <Switch>
+        <Route exact path='/' component={HomePage}></Route>
+      </Switch>
+    </div>
   );
 }
+
 
 export default App;
---
+++ src/components/header/Header-component.jsx
@@ -0,0 +1,56 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+// Header.jsx
+//
+// This component redners the header for the portfolio app.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+// Webpack will do the right thing with these images:
+// https://create-react-app.dev/docs/adding-images-fonts-and-files
+import GithubImage from './GitHub-Mark-Light-32px.png';
+import LinkedInImage from './linkedin.jpeg';
+import MediumImage from './medium.jpeg'
+
+import './Header-component.scss';
+
+
+
+// This function component renders the header for the portfolio app.
+//
+const Header = () => {
+
+    return (
+        <div className='header-container'>
+            <div>
+                <h3 className='brand'>Scott Lurowist</h3>
+            </div>
+            <div className='follow-me'>
+                <Link href='' className='social-link'>Portfolio</Link>
+                <span>|</span>
+                <a href='https://github.com/scottlurowist' className='social-link'>
+                    <img src={GithubImage} alt='GitHub'
+                        title='Follow me on GitHub'
+                        width='32' height='32'/>
+                </a>
+                <a href='https://www.linkedin.com/in/slurowistnynet' className='social-link'>
+                    <img src={LinkedInImage} alt='GitHub'
+                        title='Follow me on LinkedIn'
+                        width='32' height='32'/>
+                </a>
+                <a href='https://medium.com/@scottlurowist' className='social-link'>
+                    <img src={MediumImage} alt='GitHub'
+                        title='Follow me on LinkedIn'
+                        width='40' height='40'/>
+                </a>                
+            </div>
+        </div>
+    )
+};
+
+
+export default Header;
