960277a7c40670c7acbf8ebcb9ea55316c2b36c1
scottlurowist <scottlurowist@gmail.com>
2020-10-17 13:04:01 -0400

    I HAVE A BASIC HOMEPAGE WITH MY TYPING TEXT
    I had to remove the exact path for the homepage. It caused the HomePage
    component to not route and render.
    
    I removed the <Link> in the header component since it was causing an
    error in the console.
    
    My homepage now has the scrolling text like https://caferati.me. This
    will need to become a component in my next commit.

--- src/pages/home/HomePage-component.jsx
+++ src/pages/home/HomePage-component.jsx
@@ -10,9 +10,40 @@
 import React from 'react';
 
 
-const HomePage = () => {
-    return <div>Home Page</div>;
-};
+
+
+class HomePage extends React.Component {
+
+    constructor() {
+        super();
+
+        this.textToRender = 
+            "Hello. My name is Scott Lurowist and I recently reimagined my career as a fullstack software developer."
+        this.state = {
+            renderedText: ''
+        };
+    }
+
+    typeText = () => {
+        const textArray = this.textToRender.split('');
+
+        setInterval(() => {
+            this.setState({renderedText: this.state.renderedText + textArray.splice(0, 1)});
+        }, 100);    
+    };
+
+    componentDidMount() {
+        this.typeText();
+    }
+
+    render() {
+        
+        return (
+            <span>{this.state.renderedText.toString()}</span>
+        );
+    }
+}
+
 
 
 export default HomePage;
