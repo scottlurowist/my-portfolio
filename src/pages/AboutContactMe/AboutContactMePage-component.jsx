d9254bd782f9703851dbca5b0f8535c036da0386
scottlurowist <scottlurowist@gmail.com>
2020-10-21 18:43:11 -0400

    THE NAVBAR UPDATED WITH SKILLS AND CONTACTABOUT
    I renamed ContactMePage to AboutContactMePage to save space on the
    navbar.
    
    I created an empty SkillsPage component.
    
    I added the new Skills and About/Contact links to the navbar.
    
    I added the renamed page route to App.jsx and the new Skills route.

---
+++ src/pages/AboutContactMe/AboutContactMePage-component.jsx
@@ -0,0 +1,21 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+// AboutContactMePage-component.jsx
+//
+// This component renders the About / Contact me page for the portfolio app.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+
+import React from 'react';
+
+
+
+
+const AboutContactMePage = () => {
+
+    return (<h2>About / Contact Me - this is a temp placeholder</h2>);
+};
+
+
+export default AboutContactMePage;
