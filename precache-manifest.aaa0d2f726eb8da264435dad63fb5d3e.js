7953f743ebb1d0bf30ae8b45ed76f5a7464a67ed
scottlurowist <scottlurowist@gmail.com>
2020-10-22 14:14:15 -0400

    Updates

---
+++ precache-manifest.aaa0d2f726eb8da264435dad63fb5d3e.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de145bbfd1a9d1b7675916d450daafe7",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "352c4313617cde7b5a53",
+    "url": "/my-portfolio/static/css/main.b5f3c310.chunk.css"
+  },
+  {
+    "revision": "e141093cd0fcdc85dca5",
+    "url": "/my-portfolio/static/js/2.681462ec.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/my-portfolio/static/js/2.681462ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "352c4313617cde7b5a53",
+    "url": "/my-portfolio/static/js/main.70669a2e.chunk.js"
+  },
+  {
+    "revision": "f241adf7bba56ceeeb82",
+    "url": "/my-portfolio/static/js/runtime-main.8b00eace.js"
+  },
+  {
+    "revision": "405623509077784122c8743e658d0dd0",
+    "url": "/my-portfolio/static/media/800px-HTML5_logo_and_wordmark.svg.40562350.png"
+  },
+  {
+    "revision": "54d4c686dbd729b3fab40c4448bcaba9",
+    "url": "/my-portfolio/static/media/Sass-01.54d4c686.54d4c686.png"
+  },
+  {
+    "revision": "6b19ab42d4444af9d9ede2fd586e37b9",
+    "url": "/my-portfolio/static/media/css3.6b19ab42.png"
+  },
+  {
+    "revision": "f04e47156f285007a0867b5093c14064",
+    "url": "/my-portfolio/static/media/mongodb-logo.f04e4715.png"
+  },
+  {
+    "revision": "88855eb63c21c5759db11ff385ad2fa7",
+    "url": "/my-portfolio/static/media/nodejs-logo.88855eb6.png"
+  },
+  {
+    "revision": "6501c7a62fd9cdc3852ab6dc554d91c4",
+    "url": "/my-portfolio/static/media/python-logo@2x.6501c7a6.png"
+  },
+  {
+    "revision": "13368578740b7cfc459e54004312bc14",
+    "url": "/my-portfolio/static/media/react-logo-png-transparent.13368578.png"
+  }
+]);
